04cfb3024bb98e5b874ab21b862a733ae79006a8
YunDongHyeon <ydh2338@naver.com>
2017-10-26 16:10:28 +0900

    한글 한글자를 받아 초성 중성 종성을 분리하는 함수(parse(char input)) 구현 parse함수를 이용하여 두 단어를 비교하는 parseWord함수 구현 parseWord함수를 이용하여 문장 의 유사도를 비교하여 일치하는 정도를 백분율로 반환하는 함수 구현

--- app/src/main/java/com/naver/naverspeech/collathon/GameScore.java
+++ app/src/main/java/com/naver/naverspeech/collathon/GameScore.java
@@ -5,5 +5,45 @@ package com.naver.naverspeech.collathon;
  */
 
 public class GameScore {
-    
+    public int[] parse(char input) {
+        char sample;
+        int[] charParsed = new int[3];
+        sample = input;
+        sample = (char) (sample - 0xac00);
+        charParsed[2] = sample % 28;
+        charParsed[1] = ((sample - charParsed[2]) / 28) % 21;
+        charParsed[0] = (((sample - charParsed[2]) / 28) - charParsed[1]) / 21;
+        return charParsed;
+    }
+
+    public int parseWord(String orign,String input) {
+        int[] orignParse,inputParse;
+        int equalCount=0;
+        for (int i = 0; i < input.length(); i++) {
+            orignParse=parse(orign.charAt(i));
+            inputParse=parse(input.charAt(i));
+            for(int j =0;j<orignParse.length;j++) {
+                if(orignParse[j]==inputParse[j])
+                    equalCount++;
+            }
+        }
+        return equalCount;
+    }
+
+    public double parseSentence(String orign,String input) {
+        int totalLength=0,equalCount=0;
+        String[] inputSub = input.split(" ");
+        String[] orignSub = orign.split(" ");
+        if(inputSub.length!=orignSub.length) {
+            return -1;
+        }
+        for (int i = 0; i < inputSub.length; i++) {
+            totalLength+=3*inputSub[i].length();
+            equalCount+=parseWord(orignSub[i],inputSub[i]);
+        }
+        System.out.println(totalLength);
+        System.out.println(equalCount);
+        double result=equalCount/totalLength;
+        return (double)equalCount/(double)totalLength*100;
+    }
 }
